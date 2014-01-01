3879b61a90c3d2ee3de3f73f24e64e1d54361ec7
Christopher Mann <christopher.mann@gmail.com>
2014-01-01 11:39:33 -0800

    Stub library class.

---
+++ ArchiveR3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+class ArchiveR3:
+    """ Library class for the ArchiveR3 backup, verify, and restore system. """
+
+    def __init__(self):
+        self.init_vars()
+
+    def init_vars(self):
+        """ Initialize class variables. """
+
+    def main(self):
+        """ If you call the python as a script, this is what gets executed. """
+
+
+if __name__ == '__main__':
+    ArchiveR3 = ArchiveR3()
