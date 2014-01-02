dccace8d7527d95cf882b4d5c30a2dfb5d6d0017
Christopher Mann <christopher.mann@gmail.com>
2014-01-01 18:26:21 -0800

    Converted ArchiveR3.py back from a class to a regular set of function definitions to simplify the namespace during import.  Added command-line options to backup.py.

--- ArchiveR3.py
+++ ArchiveR3.py
@@ -1,38 +1,48 @@
 #!/usr/bin/env python
 
 import ConfigParser
+import sys
+import time
 
 
-class ArchiveR3:
-    """ Library class for the ArchiveR3 backup, verify, and restore system. """
+def config_read():
+    """ Read the configuration. """
+    config = ConfigParser.RawConfigParser()
+    config.read('config')
+    config.archives = config.get('ArchiveR3', 'archives')
+    config.backup_dir = config.get('ArchiveR3', 'backup_dir')
+    config.log_dir = config.get('ArchiveR3', 'log_dir')
+    config.data_dir = config.get('ArchiveR3', 'data_dir')
+    config.stale_age = config.getint('ArchiveR3', 'stale_age')
+    config.password_base = config.get('ArchiveR3', 'password_base')
+    return config
 
-    def __init__(self):
-        self.init_vars()
+def print_header(activity):
+    """ Display the start time of the activity and return the it for later
+    reference. """
+    time_init = time.time()
+    print '*' * 79
+    print
+    print 'START: ' + activity + ' - ' + \
+        time.strftime("%B %d, %Y %H:%M:%S", time.localtime(time_init))
+    print
+    print '*' * 79
+    return time_init
 
-    def init_vars(self):
-        """ Initialize class variables. """
+def print_footer(activity, time_init):
+    time_final = time.time()
+    print '*' * 79
+    print
+    print 'END: ' + activity + ' - ' + \
+          time.strftime("%B %d, %Y %H:%M:%S", time.localtime(time_final))
+    print
+    status_item('elapsed')
+    status_result(str(int(time_final - time_init)) + ' seconds')
+    print
+    print '*' * 79
 
-    def config_read(self):
-        """ Read the configuration. """
-        self.config = ConfigParser.RawConfigParser()
-        self.config.read('config')
-        self.config.archives = self.config.get('ArchiveR3', 'archives')
-        self.config.backup_dir = self.config.get('ArchiveR3', 'backup_dir')
-        self.config.log_dir = self.config.get('ArchiveR3', 'log_dir')
-        self.config.data_dir = self.config.get('ArchiveR3', 'data_dir')
-        self.config.stale_age = self.config.getint('ArchiveR3', 'stale_age')
-        self.config.password_base = self.config.get('ArchiveR3',
-                                                    'password_base')
+def status_item(item):
+    sys.stdout.write('%22s: ' % item)
 
-    def status_item(self, item):
-        sys.stdout.write('%22s: ' % item)
-
-    def status_result(self, result):
-        print result
-
-    def main(self):
-        """ If you call the python as a script, this is what gets executed. """
-
-
-if __name__ == '__main__':
-    ArchiveR3 = ArchiveR3()
+def status_result(result):
+    print result
--- backup.py
+++ backup.py
@@ -3,22 +3,58 @@
 
 # special shebang avoids output buffering
 
+from ArchiveR3 import *
+import argparse
+import sys
+
 
 class backup:
     """ Perform a backup via rsync to an encrypted filesystem hosted in the
     cloud. """
 
     def __init__(self):
+        self.config = config_read()
         self.init_vars()
-        self.ArchiveR3 = ArchiveR3.ArchiveR3()
-        self.ArchiveR3.config_read()
-        self.init_vars()
+
+    def args_process(self):
+        """ Process command-line arguments. """
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description='Verify a backup has integrity by comparing local '
+            'files against remote ones.  The contents of each file are '
+            'inspected, as well as the presense or absense of local and '
+            'remote files.  The state of both local archive and remote '
+            'backup are stored.  This state is used later in order to speed '
+            'up the validation process.  For example, this tool can '
+            'preferentially validate the files whose previously known state '
+            'is the oldest.')
+        parser.add_argument('config', action='store',
+                            help='Specify an ArchiveR3 config file.')
+        parser.add_argument('-v', dest='verbose', action='store_true',
+                            help='Print the status of each file as it is '
+                            'being processed.  Otherwise, a progress dot is '
+                            'printed for every 100 files processed.  '
+                            '\'processing\' refers not just to backing up '
+                            'files, but also checking to see if they have '
+                            'previously been backed up.')
+        if len(sys.argv) == 1:
+            parser.print_help()
+            sys.exit(1)
+        self.args = parser.parse_args()
 
     def init_vars(self):
         """ Initialize class variables. """
 
+    def backup(self):
+        status_item('Performing backups now')
+        status_result('TBD')
+
     def main(self):
         """ If you call the python as a script, this is what gets executed """
+        self.args_process()
+        time_init = print_header('backup')
+        self.backup()
+        print_footer('backup', time_init)
 
 
 if __name__ == '__main__':
